51a42bad6b784acfcd5d88f9401d3e93694d1373
Mel/ <tchoikuemel06@gmail.com>
2024-04-25 00:10:14 +0100

    Add fake data mirajs for vans page

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from 'react';
+
 const Vans = () => {
-  return (
-    <h1>Vans goes here</h1>
-  )
-}
-export default Vans
+	const [vans, setVans] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const response = await fetch('/api/van'); // Assurez-vous que l'URL correspond à celle configurée dans votre serveur MirageJS
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
+				const data = await response.json();
+				console.log(data.vans);
+				setVans(data.vans);
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			}
+		}
+
+		fetchData();
+	}, []);
+
+	return (
+		<>
+			{vans.map((van) => (
+				<p key={van.id}>{van.name}</p>
+			))}
+		</>
+	);
+};
+export default Vans;
