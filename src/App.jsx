b967f1cf52ff4361dbf681a300fcc67460270835
Mel/ <tchoikuemel06@gmail.com>
2024-04-24 02:41:09 +0100

    refactorie router in to an  app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
+const Home = () => {
+	return <h1>Home page</h1>;
+};
+
+const About = () => {
+	return <h1>About page</h1>;
+};
+const Products = () => {
+	return <h1>Product page</h1>;
+};
+
 const App = () => {
-  return (
-    <h1>Hello world</h1>
-  )
-}
-export default App
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/about' element={<About />} />
+			<Route path='/products' element={<Products />} />
+		</Routes>
+	);
+};
+export default App;
