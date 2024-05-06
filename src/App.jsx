b7a86188665579359ee68961364155965427f878
Mel/ <tchoikuemel06@gmail.com>
2024-05-06 03:06:12 +0100

    added outlet in the host route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Vans from './pages/Vans';
-import VanDetail from './pages/VanDetail';
+import Vans from './pages/Vans/Vans';
+import VanDetail from './pages/Vans/VanDetail';
 import Layout from './Components/Layout';
-import Host from './pages/Host';
+
 import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
@@ -13,15 +13,18 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
+				{/* parent road nav and footer component  */}
 				<Route element={<Layout />}>
 					<Route path='/' element={<Home />} />
 					<Route path='/about' element={<About />} />
-					<Route path='/host' element={<Host />} />
-					<Route path='/host/dashboard' element={<Dashboard />} />
-					<Route path='/host/reviews' element={<Reviews />} />
-					<Route path='/host/income' element={<Income />} />
 					<Route path='/vans' element={<Vans />} />
 					<Route path='/vans/:id' element={<VanDetail />} />
+					
+					{/* host nesting */}
+					<Route path='/host' element={<Dashboard />}>
+						<Route path='/host/reviews' element={<Reviews />} />
+						<Route path='/host/income' element={<Income />} />
+					</Route>
 				</Route>
 			</Routes>
 		</BrowserRouter>
--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,6 +1,15 @@
+import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 const Dashboard = () => {
-  return (
-    <h1>Dashboard page there</h1>
-  )
-}
-export default Dashboard
+	return (
+		<>
+			<h1>Host page there</h1>
+			<div>
+				<Link to='/host/reviews'>Reviews</Link>{' '}
+				<Link to='/host/income'>Income</Link>
+			</div>
+			<Outlet />
+		</>
+	);
+};
+export default Dashboard;
