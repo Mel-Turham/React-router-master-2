5a22c0fe6f41bc553e79d7f32baa2366f64662fa
Mel/ <tchoikuemel06@gmail.com>
2024-05-08 17:31:53 +0100

    add router provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Vans from './pages/Vans/Vans';
@@ -18,39 +23,37 @@ import Pricing from './pages/Host/Pricing';
 import Photos from './pages/Host/Photos';
 import NotFound from './pages/NotFound';
 
-const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				{/* parent road nav and footer component  */}
-				
-				<Route path='/' element={<Layout />}>
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		// {/* parent road nav and footer component  */}
+		<Route path='/' element={<Layout />}>
+			<Route index element={<Home />} />
+			<Route path='about' element={<About />} />
+			<Route path='vans' element={<Vans />} />
+			<Route path='vans/:id' element={<VanDetail />} />
 
-					<Route index element={<Home />} />
-					<Route path='about' element={<About />} />
-					<Route path='vans' element={<Vans />} />
-					<Route path='vans/:id' element={<VanDetail />} />
+			{/* host nesting */}
 
-					{/* host nesting */}
+			<Route path='host' element={<HostLayout />}>
+				<Route index element={<Dashboard />} />
+				<Route path='income' element={<Income />} />
+				<Route path='reviews' element={<Reviews />} />
+				<Route path='vans' element={<HostVans />} />
 
-					<Route path='host' element={<HostLayout />}>
-						<Route index element={<Dashboard />} />
-						<Route path='income' element={<Income />} />
-						<Route path='reviews' element={<Reviews />} />
-						<Route path='vans' element={<HostVans />} />
-
-						<Route path='vans/:id' element={<HostVansDetail />}>
-							<Route index element={<HostVansInfo />} />
-							<Route path='pricing' element={<Pricing />} />
-							<Route path='photos' element={<Photos />} />
-						</Route>
-					</Route>
-
-					{/* 404 page */}
-					<Route path='*' element={<NotFound />} />
+				<Route path='vans/:id' element={<HostVansDetail />}>
+					<Route index element={<HostVansInfo />} />
+					<Route path='pricing' element={<Pricing />} />
+					<Route path='photos' element={<Photos />} />
 				</Route>
-			</Routes>
-		</BrowserRouter>
-	);
+			</Route>
+
+			{/* 404 page */}
+			<Route path='*' element={<NotFound />} />
+		</Route>,
+	),
+);
+
+const App = () => {
+	return <RouterProvider router={router} />;
 };
 export default App;
